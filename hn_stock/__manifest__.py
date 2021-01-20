f4a227ebdbee67293a82934940d3c0bb07268f18
Amirul <muhammad.amirul.muk@gmail.com>
2021-01-20 08:24:41 +0700

    add description to addons

--- hn_stock/__manifest__.py
+++ hn_stock/__manifest__.py
@@ -5,6 +5,8 @@
     'summary': 'Custom Stock, Product',
     'website': '',
     'description': """
+        custom:
+        1. only show cost product to user accounting and purchase
     """,
     'data': [
         'views/product_product_view.xml'
