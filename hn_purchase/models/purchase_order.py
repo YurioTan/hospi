2f7b7deacc976f56d506aa9e50f0360d2e31416b
Server Development Local Juned <server@local.juned.id>
2024-02-28 15:30:01 +0700

    autofill line description on stock picking from PO

--- hn_purchase/models/purchase_order.py
+++ hn_purchase/models/purchase_order.py
@@ -7,6 +7,5 @@ class PurchaseOrderLine(models.Model):
     def _prepare_stock_move_vals(self, picking, price_unit, product_uom_qty, product_uom):
         # force PO line's description as picking line's description
         res = super(PurchaseOrderLine, self)._prepare_stock_move_vals(picking, price_unit, product_uom_qty, product_uom)
-        raise UserError("%s %s" % (res, self.name))
         res['description_picking'] = self.name
         return res
