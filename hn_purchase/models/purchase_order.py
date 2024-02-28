6d43ea0959afb697c6b7128d1907ceb53945de8e
Server Development Local Juned <server@local.juned.id>
2024-02-28 14:32:42 +0700

    autofill line description on stock picking from PO

--- hn_purchase/models/purchase_order.py
+++ hn_purchase/models/purchase_order.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-
+from odoo.exceptions import ValidationError,UserError
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
@@ -29,4 +29,19 @@ class PurchaseOrder(models.Model):
         new_seq = '/'.join(new_seq)
         vals['name'] = new_seq
         res = super(PurchaseOrder, self).create(vals)
+        return res
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.multi
+    def _prepare_stock_moves(self, picking):
+        res = super(PurchaseOrderLine, self)._prepare_stock_moves(picking)
+        debug = ""
+        for record in res:
+            debug += "%s" % record
+            # if record['purchase_line_id']:
+            #     purchase_line = self.env['purchase.order.line'].browse(record['purchase_line_id'])
+            #     record['']
+        raise ValidationError(debug)
         return res
