de655bbeb3977cab59a6480765b1f1e70026bb69
Server Development Local Juned <server@local.juned.id>
2024-09-11 19:44:47 +0700

    tambah field area map dan level ke customer

--- arkana_base_crm/models/partner_setup.py
+++ arkana_base_crm/models/partner_setup.py
@@ -28,6 +28,20 @@ class PartnerLocation(models.Model):
     active = fields.Boolean(default=True)
     name = fields.Char("Name", index=True)
     
+class PartnerAreaMap(models.Model):
+    _name = "partner.area.map"
+    _description = "Partner Area Map"
+
+    active = fields.Boolean(default=True)
+    name = fields.Char("Name", index=True)
+    
+class PartnerLevel(models.Model):
+    _name = "partner.level"
+    _description = "Partner Level"
+
+    active = fields.Boolean(default=True)
+    name = fields.Char("Name", index=True)
+    
 class Partner(models.Model):
     _inherit = 'res.partner'
     _description = 'Partner'
@@ -39,6 +53,10 @@ class Partner(models.Model):
     partner_category_id = fields.Many2one('partner.category', string='Wilayah',
                     index = True, copy = False, tracking = True)
     partner_class_id = fields.Many2one('partner.class', string='Kelas RS',
+                    index = True, copy = False, tracking = True)
+    partner_area_map_id = fields.Many2one('partner.area.map', string='Peta Area',
+                    index = True, copy = False, tracking = True)
+    partner_level_id = fields.Many2one('partner.level', string='Level',
                     index = True, copy = False, tracking = True)
     alias_name = fields.Char('Alias Name', tracking = True, 
                             copy = False, index = True)
