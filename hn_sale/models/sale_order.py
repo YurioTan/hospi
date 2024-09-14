16a41eed4996bd3118434c916cad45f7b960d701
Server Development Local Juned <server@local.juned.id>
2024-09-14 12:18:09 +0700

    wip modif SO for customer db

--- hn_sale/models/sale_order.py
+++ hn_sale/models/sale_order.py
@@ -2,38 +2,50 @@ from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 
 class SaleOder(models.Model):
-    _inherit = 'sale.order'
+  _inherit = 'sale.order'
 
-    # deprecated as end of 202312, replaced by order_type
-    co_sale = fields.Boolean('CO', default=False)
+  # deprecated as end of 202312, replaced by order_type
+  co_sale = fields.Boolean('CO', default=False)
 
-    order_type = fields.Selection([
-        ('oem', 'OEM'),
-        ('service', 'Service'),
-        ('distributor', 'Distributor'),
-        ('swasta', 'Swasta'),
-        ('institusi', 'Institusi'),
-        ('reguler', 'Reguler'),
-    ], string='Order Type', required=True, default="reguler")
-    distributor_id = fields.Many2one('res.partner', 'Distributor', domain=[('customer_type','=','distributor')])
+  order_type = fields.Selection([
+    ('oem', 'OEM'),
+    ('service', 'Service'),
+    ('distributor', 'Distributor'),
+    ('swasta', 'Swasta'),
+    ('institusi', 'Institusi'),
+    ('reguler', 'Reguler'),
+  ], string='Order Type', required=True, default="reguler")
+  distributor_id = fields.Many2one('res.partner', 'Distributor', domain=[('customer_type','=','distributor')])
 
-    @api.model
-    def create(self, vals):
-        order_type = vals.get('order_type')
-        if order_type == 'oem':
-            vals['name'] = self.env['ir.sequence'].next_by_code('co.sale.sequence')
-        elif order_type == 'service':
-            vals['name'] = self.env['ir.sequence'].next_by_code('service.sale.sequence')
-        elif order_type == 'distributor':
-            vals['name'] = self.env['ir.sequence'].next_by_code('distributor.sale.sequence')
-        elif order_type == 'swasta':
-            vals['name'] = self.env['ir.sequence'].next_by_code('swasta.sale.sequence')
-        elif order_type == 'institusi':
-            vals['name'] = self.env['ir.sequence'].next_by_code('institusi.sale.sequence')
-        elif order_type == 'reguler':
-            vals['name'] = self.env['ir.sequence'].next_by_code('sale.order')
-        else:
-            raise ValidationError(_("Invalid order type. Please make sure to create/edit orders from their corresponding menus."))
+  @api.model
+  def create(self, vals):
+    order_type = vals.get('order_type')
+    if order_type == 'oem':
+      vals['name'] = self.env['ir.sequence'].next_by_code('co.sale.sequence')
+    elif order_type == 'service':
+      vals['name'] = self.env['ir.sequence'].next_by_code('service.sale.sequence')
+    elif order_type == 'distributor':
+      vals['name'] = self.env['ir.sequence'].next_by_code('distributor.sale.sequence')
+    elif order_type == 'swasta':
+      vals['name'] = self.env['ir.sequence'].next_by_code('swasta.sale.sequence')
+    elif order_type == 'institusi':
+      vals['name'] = self.env['ir.sequence'].next_by_code('institusi.sale.sequence')
+    elif order_type == 'reguler':
+      vals['name'] = self.env['ir.sequence'].next_by_code('sale.order')
+    else:
+      raise ValidationError(_("Invalid order type. Please make sure to create/edit orders from their corresponding menus."))
 
-        res = super(SaleOder, self).create(vals)
-        return res
+    res = super(SaleOder, self).create(vals)
+    return res
+
+  @api.multi
+  def action_create_customer_db(self):
+    for row in self:
+      return True
+
+  @api.multi
+  def action_view_customer_db(self):
+    xmlid = "hn_sale.action_sale_customer_db"
+    action = self.env["ir.actions.act_window"]._for_xml_id(xmlid)
+    action['domain'] = [('sale_order_id','=',self.id)]
+    return action
